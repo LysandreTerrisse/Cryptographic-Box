053e3f5986835a6d7a01141187c02d1e7507d659
Lysandre Terrisse <124552354+LysandreTerrisse@users.noreply.github.com>
2026-07-20 12:47:03 +0200

    Compute the counter only once

--- MPC/boolean_circuits.py
+++ MPC/boolean_circuits.py
@@ -1,4 +1,26 @@
-"""The following functions form a boolean circuit to count whether an array of bits has n elements"""
+# Takes a counter, that is, a list of bits, least significant bit first
+# This function increases the counter by 1 if and only if x is true
+def add_bit(counter, x):
+    carry = x
+    for i, bit in enumerate(counter):
+        counter[i], carry = bit ^ carry, bit & carry
+
+# bits is a list of encrypted bits
+# Creates a counter and adds every encrypted bit to it
+def count(bits):
+    counter_size = len(bits).bit_length()
+    counter = [0]*counter_size
+    for x in bits:
+        add_bit(counter, x)
+    return counter
+
+# Compares a counter to an integer
+def equal(counter, n):
+    equal = 1
+    for i in range(len(counter)):
+        # We compare the i-th bit of the counter with the i-th bit of n
+        equal &= (counter[i] ^ bool((n >> i) & 1)) ^ 1
+    return equal"""The following functions form a boolean circuit to count whether an array of bits has n elements"""
 
 # Takes a counter, that is, a list of bits, least significant bit first
 # This function increases the counter by 1 if and only if x is true
