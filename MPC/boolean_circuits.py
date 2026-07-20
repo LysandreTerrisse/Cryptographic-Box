d7cfe7c8bb8a25f5ef7e108e8208bf5657289e31
Lysandre Terrisse <124552354+LysandreTerrisse@users.noreply.github.com>
2026-07-20 12:47:34 +0200

    Fixed copy-paste

--- MPC/boolean_circuits.py
+++ MPC/boolean_circuits.py
@@ -20,27 +20,4 @@ def equal(counter, n):
     for i in range(len(counter)):
         # We compare the i-th bit of the counter with the i-th bit of n
         equal &= (counter[i] ^ bool((n >> i) & 1)) ^ 1
-    return equal"""The following functions form a boolean circuit to count whether an array of bits has n elements"""
-
-# Takes a counter, that is, a list of bits, least significant bit first
-# This function increases the counter by 1 if and only if x is true
-def add_bit(counter, x):
-    carry = x
-    for i, bit in enumerate(counter):
-        counter[i], carry = bit ^ carry, bit & carry
-
-# bits is a list of encrypted bits
-# Returns whether exactly n of the boolean inputs are True.
-def count(bits, n):
-    counter_size = len(bits).bit_length()
-    counter = [0]*counter_size
-    for x in bits:
-        add_bit(counter, x)
-    
-    expected_counter = [bool((n >> i) & 1) for i in range(counter_size)]
-
-    equal = 1
-    for a, b in zip(counter, expected_counter):
-        equal &= (a ^ b) ^ 1
-
     return equal
