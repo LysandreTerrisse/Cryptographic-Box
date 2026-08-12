fe7836da54891e62b49bea8a8064f4e6a6f90601
Lysandre Terrisse <124552354+LysandreTerrisse@users.noreply.github.com>
2026-08-12 05:15:37 +0200

    Optimized the rotate function

--- Turing/turing_machine_3_semilinear.py
+++ Turing/turing_machine_3_semilinear.py
@@ -113,17 +113,15 @@ def phase(j):
         expand(j)
         expand(j)
 
-def rotate(begin, end, amount, is_left, is_right, is_center):
-    length = end - begin + 1
+def rotate(begin, end, amount, is_left, is_right):
     # We compute the three possible rotations
-    left = [tape[begin + (i + amount) % length] for i in range(length)]
-    right = [tape[begin + (i - amount) % length] for i in range(length)]
     center = tape[begin:end + 1]
-    # We mask the (in)correct ones
+    left = center[amount:] + center[:amount]
+    right = center[-amount:] + center[:-amount]
     # We perform the left rotation if the head is to the right
     # We perform the right rotation if the head is to the left
     # We perform no rotation if the head is to the center
-    tape[begin:end + 1] = [if_then_else(is_right, left[i], if_then_else(is_left, right[i], center[i])) for i in range(length)]
+    tape[begin:end + 1] = [if_then_else(is_right, left[i], if_then_else(is_left, right[i], center[i])) for i in range(len(center))]
 
 def compress(j):
     # The origin is at len(tape)//2
@@ -137,18 +135,18 @@ def compress(j):
     # We consider the cells in a 2^(j+1) - 1 radius around the origin
     # We do a 2^(j-1)-shift towards the direction where the head isn't.
     # In the case where the head is at the origin, we do nothing
-    rotate(begin = origin - (2**(j+1) - 1), end = origin + (2**(j+1) - 1), amount = 2**(j-1), is_left=is_left, is_right=is_right, is_center=is_center)
+    rotate(begin = origin - (2**(j+1) - 1), end = origin + (2**(j+1) - 1), amount = 2**(j-1), is_left=is_left, is_right=is_right)
     # We add a note to the stack that tells the position where the head was
-    stack.append((is_left, is_right, is_center))
+    stack.append((is_left, is_right))
 
 def expand(j):
     # We get the note
-    (was_left, was_right, was_center) = stack.pop()
+    (was_left, was_right) = stack.pop()
     # We do the opposite direction
     origin = len(tape)//2
-    rotate(begin = origin - (2**(j+1) - 1), end = origin + (2**(j+1) - 1), amount = 2**(j-1), is_left=was_right, is_right=was_left, is_center=was_center)
+    rotate(begin = origin - (2**(j+1) - 1), end = origin + (2**(j+1) - 1), amount = 2**(j-1), is_left=was_right, is_right=was_left)
 
-tape = [0 for _ in range(2**7 - 1)]#range(30000)]
+tape = [0 for _ in range(2**7 - 1)]
 tape[len(tape)//2] = 1 # We set the LSB to 1 to tell that the head is here
 tape = encrypt(tape)
 
@@ -156,7 +154,7 @@ stack = []
 
 #code = "1RB1LB_1LA1RC_0RC1LC"
 #code = "1RB1RD_1LB0RC_1LC1LA_0RD1LD"
-code = "1RB1LB_1LA0LC_1RE1LD_1RD0RA_0RE1LE"
+#code = "1RB1LB_1LA0LC_1RE1LD_1RD0RA_0RE1LE"
 code = "1RB1LC_1RC1RB_1RD0LE_1LA1LD_1RZ0LA_0RZ1LZ"
 table = get_transition_table(code)
 
